3b38dda475f0aadf8083263eea19cc9bb21957b0
TaylorDarneille <tmdarneille@dons.usfca.edu>
2018-02-20 10:16:26 -0800

    added goHomeCalled to state and goHome function to Nav.js; got navbar to rerender but still dropdown does not reset to default value when brand logo is clicked and home page is rendered

--- src/Nav.js
+++ src/Nav.js
@@ -4,17 +4,32 @@ import SORTS from './SORTS.js';
 
 class Nav extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			goHomeCalled: 0
+		}
+	}
+
 	selectSort = (e) => {
 		this.props.selectSort(e.target.value);
 	}
+
+	goHome = () => {
+		console.log("Go Home Called");
+		let count = this.state.goHomeCalled+1;
+		this.setState({goHomeCalled: count});
+		this.props.selectSort(-1);
+	} 
 
 	render(){
 		const sorts = SORTS.sorts.map((sort) => {
       		return <option value={sort.index}>{sort.title}</option>
     	});
+    	console.log("this.state.goHomeCalled: "+this.state.goHomeCalled);
 		return(
 			<nav>
-				<span className="brand">Learn2Sort</span>
+				<button onClick={this.goHome}>Learn2Sort</button>
 				<select onChange={this.selectSort}>
 					<option selected="selected" value="-1">Sorts</option>
 					{sorts}
